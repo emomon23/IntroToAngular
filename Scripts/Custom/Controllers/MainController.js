22d7cab2f1a6e7c1a5474b7b4c103cdaaf5c1c52
emomon23 <memo@iemosoft.com>
2014-06-29 08:46:27 -0500

    Get data from WebAPI Products Controller. (No more 'dummyData')

--- Scripts/Custom/Controllers/MainController.js
+++ Scripts/Custom/Controllers/MainController.js
@@ -1,38 +1,19 @@
-//Dummy Data!
-var dummyData = {
-    promotionalMessage : "Fishing Lures on sale, 50% off!",
-    products: [
-                { name: "DD Blake and Decker Drill", price: 43.23, description: "Mid level drill (Dummy data)", category: "Tools" },
-                { name: "DD Mikita Cordless Saw", price: 159.72, description: "3.5 inch cordless saw (Dummy data)", category: "Tools" },
-                { name: "DD Rapala Copper #4", price: 9.89, description: "crank bait, 8-12 feet (Dummy data)", category: "Fishing Lures" },
-                { name: "DD Rapala Fire Tiger #6", price: 9.89, description: "crank bait, 8-12 feet (Dummy data)", category: "Fishing Lures" },
-                { name: "DD Rapala Fire Tiger #14", price: 19.89, description: "crank bait, 15-22 feet (Dummy data)", category: "Fishing Lures" },
-                { name: "DD Yozury Fire Tiger", price: 42.89, description: "Japaneese crank bait, 10-17 feet (Dummy data)", category: "Fishing Lures" },
-    ]
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Dummy data removed
 
 
 angular.module("SportzApp")
-   .controller("SportzAppCTRL", function ($scope, $http) {
+   .constant('getProductListURL', 'api/products/getproducts')
+   .controller("SportzAppCTRL", function ($scope, $http, getProductListURL) {
        var currentlySelectedCategory = 'HOME';
 
-        $scope.data = dummyData
-                
+       $http.get(getProductListURL)
+       .success(function (getProductsResponse) {
+           $scope.data = getProductsResponse;
+       })
+       .error(function (error) {
+           $scope.data.heartBeatResult = error.message;
+       });
+                 
         $scope.heartBeat = function () {
             var url = 'api/SysAdmin/PingService';
             
--- Scripts/Custom/Filters/DistinctCategories.js
+++ Scripts/Custom/Filters/DistinctCategories.js
@@ -1,9 +1,13 @@
 ﻿angular.module('SportzApp')
     .filter("distinctCategories", function () {
-        return function(listOfProduct){
+        return function (listOfProduct) {
             var keys = [];
             var results = [];
 
+            if (angular.isUndefined(listOfProduct)) {
+                return results;
+            }
+           
             for (i = 0; i < listOfProduct.length; i++) {
                 var catName = listOfProduct[i].category;
 
